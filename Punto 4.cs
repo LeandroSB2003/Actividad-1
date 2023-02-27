fb83709b0d73f1ccde7d7603916b0599d6515ddc
LeandroSB2003 <126204634+LeandroSB2003@users.noreply.github.com>
2023-02-27 13:27:07 -0500

    Update Punto 4.cs

--- Punto 4.cs
+++ Punto 4.cs
@@ -1,23 +1,23 @@
-﻿string Nombre;
+string Nombre;
 double calificacion;
 
 
 Console.WriteLine("Por favor, digita el nombre del empleado:");
 Nombre = Console.ReadLine();
 Console.WriteLine("Digita la calificacion, siendo 0.0 inaceptable, 0.4 aceptable, 0.6 meritorio: ");
-calificacion = double.Parse(Console.ReadLine());
+calificacion = Double.Parse(Console.ReadLine());
 
-if(calificacion==0){
+if(calificacion==0.0){
 
 Console.WriteLine("El empleado " + Nombre + " con la calificacion " + calificacion + " no tiene bono, quedando el precio base de 50000");
 
-}else if(calificacion==4){
+}else if(calificacion==0.4){
 
 
 Console.WriteLine("El empleado " + Nombre + " con la calificacion " + calificacion + " tiene un bono del 0.4, quedando en " + (0.4*50000));
 
 
-}else if(calificacion==6){
+}else if(calificacion==0.6){
 
 Console.WriteLine("El empleado " + Nombre + " con la calificacion " + calificacion + " tiene un bono del 0.6, quedando en " + (0.6*50000));
 
